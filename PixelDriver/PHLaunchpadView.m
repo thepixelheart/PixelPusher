e29a44a936b09cc8e1894d10c006352bc0c660a9
jverkoey <jverkoey@gmail.com>
2012-12-22 18:02:01 -0800

    Match the launchpad design more closely.

--- PixelDriver/PHLaunchpadView.m
+++ PixelDriver/PHLaunchpadView.m
@@ -16,6 +16,141 @@
 
 #import "PHLaunchpadView.h"
 
+#import "PHLaunchpadMIDIDriver.h"
+
+@interface PHLaunchpadButtonCell : NSButtonCell
+@property (nonatomic, assign) PHLaunchpadColor color;
+@end
+
+@implementation PHLaunchpadButtonCell {
+  BOOL _square;
+}
+
+- (id)initTextCell:(NSString *)aString {
+  if ((self = [super initTextCell:aString])) {
+    _color = PHLaunchpadColorOff;
+  }
+  return self;
+}
+
+- (id)initSquareButton {
+  if ((self = [self initTextCell:nil])) {
+    self.backgroundColor = [NSColor redColor];
+    _square = YES;
+  }
+  return self;
+}
+
+- (id)initRoundButton {
+  if ((self = [self initTextCell:nil])) {
+    self.backgroundColor = [NSColor greenColor];
+    _square = NO;
+  }
+  return self;
+}
+
+- (NSColor *)NSColorFromLaunchpadColor:(PHLaunchpadColor)color {
+  switch (color) {
+    case PHLaunchpadColorOff:
+      return [NSColor colorWithDeviceRed:0.5 green:0.5 blue:0.5 alpha:1];
+    case PHLaunchpadColorRedDim:
+      return [NSColor colorWithDeviceRed:0.5 green:0 blue:0 alpha:1];
+    case PHLaunchpadColorRedFlashing:
+    case PHLaunchpadColorRedBright:
+      return [NSColor colorWithDeviceRed:1 green:0 blue:0 alpha:1];
+    case PHLaunchpadColorAmberDim:
+      return [NSColor colorWithDeviceRed:0.5 green:0.25 blue:0 alpha:1];
+    case PHLaunchpadColorAmberFlashing:
+    case PHLaunchpadColorAmberBright:
+      return [NSColor colorWithDeviceRed:1 green:0.5 blue:0 alpha:1];
+    case PHLaunchpadColorYellowFlashing:
+    case PHLaunchpadColorYellowBright:
+      return [NSColor colorWithDeviceRed:1 green:1 blue:0 alpha:1];
+    case PHLaunchpadColorGreenDim:
+      return [NSColor colorWithDeviceRed:0 green:0.5 blue:0 alpha:1];
+    case PHLaunchpadColorGreenFlashing:
+    case PHLaunchpadColorGreenBright:
+      return [NSColor colorWithDeviceRed:0 green:1 blue:0 alpha:1];
+    default:
+      return nil;
+  }
+}
+
+- (void)drawWithFrame:(NSRect)cellFrame inView:(NSView *)controlView {
+  [NSGraphicsContext saveGraphicsState];
+
+  [[self NSColorFromLaunchpadColor:_color] set];
+  CGContextRef cx = [[NSGraphicsContext currentContext] graphicsPort];
+
+  if (_square) {
+    CGFloat radius = 5;
+    CGRect rect = cellFrame;
+    // http://stackoverflow.com/questions/1031930/how-is-a-rounded-rect-view-with-transparency-done-on-iphone/1031936#1031936
+
+    // Top left
+    CGContextMoveToPoint(cx, rect.origin.x, rect.origin.y + radius);
+
+    // Left edge
+    CGContextAddLineToPoint(cx, rect.origin.x, rect.origin.y + rect.size.height - radius);
+
+    // Bottom left corner
+    if (self.tag == 28) {
+      CGContextAddLineToPoint(cx, rect.origin.x + radius, rect.origin.y + rect.size.height);
+    } else {
+      CGContextAddArc(cx, rect.origin.x + radius, rect.origin.y + rect.size.height - radius,
+                      radius, M_PI, M_PI / 2, 1);
+    }
+
+    // Bottom edge
+    CGContextAddLineToPoint(cx, rect.origin.x + rect.size.width - radius,
+                            rect.origin.y + rect.size.height);
+
+    // Bottom right corner
+    if (self.tag == 27) {
+      CGContextAddLineToPoint(cx, rect.origin.x + rect.size.width,
+                              rect.origin.y + rect.size.height - radius);
+    } else {
+      CGContextAddArc(cx, rect.origin.x + rect.size.width - radius,
+                      rect.origin.y + rect.size.height - radius, radius, M_PI / 2, 0.0f, 1);
+    }
+
+    // Right edge
+    CGContextAddLineToPoint(cx, rect.origin.x + rect.size.width, rect.origin.y + radius);
+
+    // Top right corner
+    if (self.tag == 35) {
+      CGContextAddLineToPoint(cx, rect.origin.x + rect.size.width - radius, rect.origin.y);
+    } else {
+      CGContextAddArc(cx, rect.origin.x + rect.size.width - radius, rect.origin.y + radius,
+                      radius, 0.0f, -M_PI / 2, 1);
+    }
+
+    // Top edge
+    CGContextAddLineToPoint(cx, rect.origin.x + radius, rect.origin.y);
+
+    // Top left corner
+    if (self.tag == 36) {
+      CGContextAddLineToPoint(cx, rect.origin.x, rect.origin.y + radius);
+    } else {
+      CGContextAddArc(cx, rect.origin.x + radius, rect.origin.y + radius, radius,
+                      -M_PI / 2, M_PI, 1);
+    }
+
+    CGContextFillPath(cx);
+  } else {
+    CGRect circleFrame = cellFrame;
+    circleFrame.origin.x += 4;
+    circleFrame.origin.y += 1;
+    circleFrame.size.width -= 9;
+    circleFrame.size.height -= 8;
+    CGContextFillEllipseInRect(cx, circleFrame);
+  }
+
+  [NSGraphicsContext restoreGraphicsState];
+}
+
+@end
+
 @implementation PHLaunchpadView
 
 - (void)awakeFromNib {
@@ -26,6 +161,12 @@
       NSButton* button = (NSButton *)view;
       button.target = self;
       button.action = @selector(didTapButton:);
+      if (button.tag < 64) {
+        button.cell = [[PHLaunchpadButtonCell alloc] initSquareButton];
+      } else {
+        button.cell = [[PHLaunchpadButtonCell alloc] initRoundButton];
+      }
+      [button.cell setTag:button.tag];
     }
   }
 }
@@ -61,8 +202,9 @@
   [super drawRect:dirtyRect];
 }
 
-- (IBAction)didTapButton:(id)sender {
-  
+- (IBAction)didTapButton:(NSButton *)sender {
+  NSLog(@"%ld", sender.tag);
+  [(NSButtonCell *)sender.cell setBackgroundColor:[NSColor redColor]];
 }
 
 @end
