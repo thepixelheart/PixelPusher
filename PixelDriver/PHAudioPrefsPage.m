58094b9da48f07de4e2ead36abe91deff2c60033
jverkoey <jverkoey@gmail.com>
2013-01-19 18:14:33 -0800

    Show audio drivers.

--- PixelDriver/PHAudioPrefsPage.m
+++ PixelDriver/PHAudioPrefsPage.m
@@ -16,6 +16,100 @@
 
 #import "PHAudioPrefsPage.h"
 
-@implementation PHAudioPrefsPage
+#import "AppDelegate.h"
+#import "PHFMODRecorder.h"
+
+static const NSEdgeInsets kContentPadding = {10, 10, 10, 10};
+
+typedef enum {
+  PHAudioPrefIdSource,
+  PHAudioPrefIdDestination,
+} PHAudioPrefId;
+
+@implementation PHAudioPrefsPage {
+  NSMutableArray* _rowsOfViewPairs;
+}
+
+- (id)initWithFrame:(NSRect)frameRect {
+  if ((self = [super initWithFrame:frameRect])) {
+    _rowsOfViewPairs = [NSMutableArray array];
+    [self addRowWithLabel:@"Audio Source" popUpButtonId:PHAudioPrefIdSource];
+    [self addRowWithLabel:@"Audio Destination" popUpButtonId:PHAudioPrefIdDestination];
+  }
+  return self;
+}
+
+#pragma mark - Layout
+
+- (void)layout {
+  [super layout];
+
+  CGSize boundsSize = self.contentView.bounds.size;
+
+  CGFloat leftColRightEdge = kContentPadding.left;
+
+  // Calculate the widest label
+  for (NSInteger ix = 0; ix < _rowsOfViewPairs.count; ix += 2) {
+    NSView* leftView = _rowsOfViewPairs[ix];
+    NSView* rightView = _rowsOfViewPairs[ix + 1];
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Warc-performSelector-leaks"
+    SEL sizeToFit = @selector(sizeToFit);
+    if ([leftView respondsToSelector:sizeToFit]) {
+      [leftView performSelector:sizeToFit];
+    }
+    if ([rightView respondsToSelector:sizeToFit]) {
+      [rightView performSelector:sizeToFit];
+    }
+#pragma clang diagnostic pop
+
+    leftColRightEdge = MAX(leftColRightEdge, kContentPadding.left + leftView.frame.size.width);
+  }
+
+  CGFloat topEdge = boundsSize.height - kContentPadding.top;
+
+  for (NSInteger ix = 0; ix < _rowsOfViewPairs.count; ix += 2) {
+    NSView* leftView = _rowsOfViewPairs[ix];
+    NSView* rightView = _rowsOfViewPairs[ix + 1];
+
+    CGFloat bottomEdge = topEdge - MAX(leftView.frame.size.height, rightView.frame.size.height);
+    CGFloat height = topEdge - bottomEdge;
+    leftView.frame = CGRectMake(kContentPadding.left, bottomEdge + floor((height - leftView.frame.size.height) / 2), leftView.frame.size.width, leftView.frame.size.height);
+    rightView.frame = CGRectMake(kContentPadding.left + leftColRightEdge + 10, bottomEdge + floor((height - rightView.frame.size.height) / 2), rightView.frame.size.width, rightView.frame.size.height);
+
+    topEdge = bottomEdge;
+  }
+}
+
+#pragma mark - Adding Rows
+
+- (void)addRowWithLabel:(NSString *)labelText popUpButtonId:(NSInteger)popUpButtonId {
+  NSTextField* label = [[NSTextField alloc] init];
+  [label setEditable:NO];
+  [label setBezeled:NO];
+  [label setBackgroundColor:nil];
+  [label setTextColor:[NSColor whiteColor]];
+  label.stringValue = labelText;
+  [self.contentView addSubview:label];
+
+  [_rowsOfViewPairs addObject:label];
+
+  NSPopUpButton* button = [[NSPopUpButton alloc] init];
+  [button addItemsWithTitles:[self popUpItemTitlesForId:popUpButtonId]];
+  [self.contentView addSubview:button];
+
+  [_rowsOfViewPairs addObject:button];
+}
+
+- (NSArray *)popUpItemTitlesForId:(NSInteger)popUpButtonId {
+  if (popUpButtonId == PHAudioPrefIdSource) {
+    return PHApp().audioRecorder.recordDriverNames;
+
+  } else if (popUpButtonId == PHAudioPrefIdDestination) {
+    return PHApp().audioRecorder.playbackDriverNames;
+  }
+  return nil;
+}
 
 @end
