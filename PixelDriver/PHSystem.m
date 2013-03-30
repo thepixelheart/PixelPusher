6df307903a355907b6eb2fda563808bc6db7fc20
jverkoey <jverkoey@gmail.com>
2013-03-29 19:58:51 -0700

    Render heart last.

--- PixelDriver/PHSystem.m
+++ PixelDriver/PHSystem.m
@@ -100,15 +100,6 @@ NSString* const PHSystemValueKey = @"PHSystemValueKey";
   [animation renderBitmapInContext:context size:wallSize];
   [animation bitmapDidFinishRendering];
 
-  if (_overlayPixelHeart) {
-    CGImageRef imageRef = [_pixelHeartTextSpritesheet imageAtX:0 y:0];
-    CGSize textSize = _pixelHeartTextSpritesheet.spriteSize;
-    CGContextDrawImage(context, CGRectMake(floorf((wallSize.width - textSize.width) / 2),
-                                           floorf((wallSize.height - textSize.height) / 2),
-                                           textSize.width, textSize.height), imageRef);
-    CGImageRelease(imageRef);
-  }
-
 //  [self drawOverlaysInContext:wallContext];
 
   return context;
@@ -165,6 +156,15 @@ NSString* const PHSystemValueKey = @"PHSystemValueKey";
   }
 
   [tick updateWallContextWithTransition:_faderTransition t:_fade];
+
+  if (_overlayPixelHeart) {
+    CGImageRef imageRef = [_pixelHeartTextSpritesheet imageAtX:0 y:0];
+    CGSize textSize = _pixelHeartTextSpritesheet.spriteSize;
+    CGContextDrawImage(tick.wallContextRef, CGRectMake(floorf((kWallWidth - textSize.width) / 2),
+                                                       floorf((kWallHeight - textSize.height) / 2),
+                                                       textSize.width, textSize.height), imageRef);
+    CGImageRelease(imageRef);
+  }
 
   for (NSValue* value in animationToContext.allValues) {
     CGContextRelease([value pointerValue]);
