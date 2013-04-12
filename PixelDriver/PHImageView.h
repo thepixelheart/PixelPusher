cda96692a4ac7c929213eee2b44bedbc9b781a70
jverkoey <jverkoey@gmail.com>
2013-04-11 22:31:35 -0700

    Add image view.

---
+++ PixelDriver/PHImageView.h
@@ -0,0 +1,13 @@
+//
+//  PHImageView.h
+//  PixelDriver
+//
+//  Created by Jeff Verkoeyen on 4/11/13.
+//  Copyright (c) 2013 Pixel Heart. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface PHImageView : NSImageView
+
+@end
---
+++ PixelDriver/PHImageView.m
@@ -0,0 +1,13 @@
+//
+//  PHImageView.m
+//  PixelDriver
+//
+//  Created by Jeff Verkoeyen on 4/11/13.
+//  Copyright (c) 2013 Pixel Heart. All rights reserved.
+//
+
+#import "PHImageView.h"
+
+@implementation PHImageView
+
+@end
