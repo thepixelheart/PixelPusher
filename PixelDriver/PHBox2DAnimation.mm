f15084edccb3dc1eb3409801e0914b165d53bc31
jverkoey <jverkoey@gmail.com>
2013-04-17 03:01:35 -0700

    Dancing man.

--- PixelDriver/PHBox2DAnimation.mm
+++ PixelDriver/PHBox2DAnimation.mm
@@ -22,16 +22,9 @@ static const CGFloat kScale = 2;
 
 @implementation PHBox2DAnimation {
   b2World *_world;
-  b2Body* _body;
-  b2RevoluteJoint* _joint;
-  NSInteger _nBodies;
-
-	b2Vec2 m_offset;
-	b2Body* m_chassis;
-	b2Body* m_wheel;
-	b2RevoluteJoint* m_motorJoint;
-	bool m_motorOn;
-	float32 m_motorSpeed;
+	b2Body* m_bodies[4];
+	b2Joint* m_joints[8];
+  CGFloat _advance;
 }
 
 - (void)dealloc {
@@ -39,193 +32,125 @@ static const CGFloat kScale = 2;
     delete _world;
   }
 }
-- (void)CreateLeg:(float32)s wheelAnchor:(const b2Vec2&)wheelAnchor
-{
-  b2Vec2 p1(5.4f * s, -6.1f * kScale);
-  b2Vec2 p2(7.2f * s, -1.2f * kScale);
-  b2Vec2 p3(4.3f * s, -1.9f * kScale);
-  b2Vec2 p4(3.1f * s, 0.8f * kScale);
-  b2Vec2 p5(6.0f * s, 1.5f * kScale);
-  b2Vec2 p6(2.5f * s, 3.7f * kScale);
-
-  b2FixtureDef fd1, fd2;
-  fd1.filter.groupIndex = -1;
-  fd2.filter.groupIndex = -1;
-  fd1.density = 1.0f;
-  fd2.density = 1.0f;
-
-  b2PolygonShape poly1, poly2;
-
-  if (s > 0.0f)
-  {
-    b2Vec2 vertices[3];
-
-    vertices[0] = p1;
-    vertices[1] = p2;
-    vertices[2] = p3;
-    poly1.Set(vertices, 3);
-
-    vertices[0] = b2Vec2_zero;
-    vertices[1] = p5 - p4;
-    vertices[2] = p6 - p4;
-    poly2.Set(vertices, 3);
-  }
-  else
-  {
-    b2Vec2 vertices[3];
-
-    vertices[0] = p1;
-    vertices[1] = p3;
-    vertices[2] = p2;
-    poly1.Set(vertices, 3);
-
-    vertices[0] = b2Vec2_zero;
-    vertices[1] = p6 - p4;
-    vertices[2] = p5 - p4;
-    poly2.Set(vertices, 3);
-  }
-
-  fd1.shape = &poly1;
-  fd2.shape = &poly2;
-
-  b2BodyDef bd1, bd2;
-  bd1.type = b2_dynamicBody;
-  bd2.type = b2_dynamicBody;
-  bd1.position = m_offset;
-  bd2.position = p4 + m_offset;
-
-  bd1.angularDamping = 5.0f;
-  bd2.angularDamping = 5.0f;
-
-  b2Body* body1 = _world->CreateBody(&bd1);
-  b2Body* body2 = _world->CreateBody(&bd2);
-
-  body1->CreateFixture(&fd1);
-  body2->CreateFixture(&fd2);
-
-  b2DistanceJointDef djd;
-
-  // Using a soft distance constraint can reduce some jitter.
-  // It also makes the structure seem a bit more fluid by
-  // acting like a suspension system.
-  djd.dampingRatio = 0.5f;
-  djd.frequencyHz = 10.0f;
-
-  djd.Initialize(body1, body2, p2 + m_offset, p5 + m_offset);
-  _world->CreateJoint(&djd);
-
-  djd.Initialize(body1, body2, p3 + m_offset, p4 + m_offset);
-  _world->CreateJoint(&djd);
-
-  djd.Initialize(body1, m_wheel, p3 + m_offset, wheelAnchor + m_offset);
-  _world->CreateJoint(&djd);
-
-  djd.Initialize(body2, m_wheel, p6 + m_offset, wheelAnchor + m_offset);
-  _world->CreateJoint(&djd);
-
-  b2RevoluteJointDef rjd;
-
-  rjd.Initialize(body2, m_chassis, p4 + m_offset);
-  _world->CreateJoint(&rjd);
-}
 
 - (id)init {
   if ((self = [super init])) {
-
-		m_offset.Set(kWallWidth / 2, kWallHeight);
-		m_motorSpeed = 4.0f;
-		m_motorOn = true;
-		b2Vec2 pivot(0.0f, 0.8f);
-
     b2Vec2 gravity(0.0f, -10.0f);
     _world = new b2World(gravity);
 
-
-		// Ground
+		b2Body* ground = NULL;
 		{
 			b2BodyDef bd;
-			b2Body* ground = _world->CreateBody(&bd);
-
-			b2EdgeShape shape;
-			shape.Set(b2Vec2(0.0f, 0.0f), b2Vec2(kWallWidth, 0.0f));
-			ground->CreateFixture(&shape, 0.0f);
-//
-//			shape.Set(b2Vec2(0.0f, 0.0f), b2Vec2(0.0f, kWallHeight));
-//			ground->CreateFixture(&shape, 0.0f);
-//
-//			shape.Set(b2Vec2(kWallWidth, 0.0f), b2Vec2(kWallWidth, kWallHeight));
-//			ground->CreateFixture(&shape, 0.0f);
+			ground = _world->CreateBody(&bd);
 		}
 
-		// Balls
-		/*for (int32 i = 0; i < 40; ++i)
-		{
-			b2CircleShape shape;
-			shape.m_radius = 0.25f;
+    b2PolygonShape shape;
+    shape.SetAsBox(1, 1);
 
-			b2BodyDef bd;
-			bd.type = b2_dynamicBody;
-			bd.position.Set(-40.0f + 2.0f * i, 0.5f);
+    b2BodyDef bd;
+    bd.type = b2_dynamicBody;
 
-			b2Body* body = _world->CreateBody(&bd);
-			body->CreateFixture(&shape, 1.0f);
-		}*/
+    bd.position.Set(-5.0f + kWallWidth / 2, -5.0f + kWallHeight / 2);
+    m_bodies[0] = _world->CreateBody(&bd);
+    m_bodies[0]->CreateFixture(&shape, 5.0f);
 
-		// Chassis
-		{
-			b2PolygonShape shape;
-			shape.SetAsBox(2.5f * kScale, 1.0f * kScale);
+    bd.position.Set(5.0f + kWallWidth / 2, -5.0f + kWallHeight / 2);
+    m_bodies[1] = _world->CreateBody(&bd);
+    m_bodies[1]->CreateFixture(&shape, 5.0f);
 
-			b2FixtureDef sd;
-			sd.density = 1.0f;
-			sd.shape = &shape;
-			sd.filter.groupIndex = -1;
-			b2BodyDef bd;
-			bd.type = b2_dynamicBody;
-			bd.position = pivot + m_offset;
-			m_chassis = _world->CreateBody(&bd);
-			m_chassis->CreateFixture(&sd);
-		}
+    bd.position.Set(5.0f + kWallWidth / 2, 5.0f + kWallHeight / 2);
+    m_bodies[2] = _world->CreateBody(&bd);
+    m_bodies[2]->CreateFixture(&shape, 5.0f);
 
-		{
-			b2CircleShape shape;
-			shape.m_radius = 1.6f * kScale;
+    bd.position.Set(-5.0f + kWallWidth / 2, 5.0f + kWallHeight / 2);
+    m_bodies[3] = _world->CreateBody(&bd);
+    m_bodies[3]->CreateFixture(&shape, 5.0f);
 
-			b2FixtureDef sd;
-			sd.density = 1.0f;
-			sd.shape = &shape;
-			sd.filter.groupIndex = -1;
-			b2BodyDef bd;
-			bd.type = b2_dynamicBody;
-			bd.position = pivot + m_offset;
-			m_wheel = _world->CreateBody(&bd);
-			m_wheel->CreateFixture(&sd);
-		}
+    b2DistanceJointDef jd;
+    b2Vec2 p1, p2, d;
 
-		{
-			b2RevoluteJointDef jd;
-			jd.Initialize(m_wheel, m_chassis, pivot + m_offset);
-			jd.collideConnected = false;
-			jd.motorSpeed = m_motorSpeed;
-			jd.maxMotorTorque = 400.0f * kScale;
-			jd.enableMotor = m_motorOn;
-			m_motorJoint = (b2RevoluteJoint*)_world->CreateJoint(&jd);
-		}
+    jd.frequencyHz = 2.0f;
+    jd.dampingRatio = 0.0f;
 
-		b2Vec2 wheelAnchor;
+    jd.bodyA = ground;
+    jd.bodyB = m_bodies[0];
+    jd.localAnchorA.Set(-5.0f + kWallWidth / 2, 0.0f);
+    jd.localAnchorB.Set(-0.5f, -0.5f);
+    p1 = jd.bodyA->GetWorldPoint(jd.localAnchorA);
+    p2 = jd.bodyB->GetWorldPoint(jd.localAnchorB);
+    d = p2 - p1;
+    jd.length = d.Length();
+    m_joints[0] = _world->CreateJoint(&jd);
 
-		wheelAnchor = pivot + b2Vec2(0.0f, -0.8f);
+    jd.bodyA = ground;
+    jd.bodyB = m_bodies[1];
+    jd.localAnchorA.Set(5.0f + kWallWidth / 2, 0.0f);
+    jd.localAnchorB.Set(0.5f, -0.5f);
+    p1 = jd.bodyA->GetWorldPoint(jd.localAnchorA);
+    p2 = jd.bodyB->GetWorldPoint(jd.localAnchorB);
+    d = p2 - p1;
+    jd.length = d.Length();
+    m_joints[1] = _world->CreateJoint(&jd);
 
-    [self CreateLeg:-kScale wheelAnchor:wheelAnchor];
-    [self CreateLeg:kScale wheelAnchor:wheelAnchor];
+    jd.bodyA = ground;
+    jd.bodyB = m_bodies[2];
+    jd.localAnchorA.Set(10.0f + kWallWidth / 2, 0.0f + kWallHeight - 4);
+    jd.localAnchorB.Set(0.5f, 0.5f);
+    p1 = jd.bodyA->GetWorldPoint(jd.localAnchorA);
+    p2 = jd.bodyB->GetWorldPoint(jd.localAnchorB);
+    d = p2 - p1;
+    jd.length = d.Length();
+    m_joints[2] = _world->CreateJoint(&jd);
 
-		m_wheel->SetTransform(m_wheel->GetPosition(), 120.0f * b2_pi / 180.0f);
-    [self CreateLeg:-kScale wheelAnchor:wheelAnchor];
-    [self CreateLeg:kScale wheelAnchor:wheelAnchor];
+    jd.bodyA = ground;
+    jd.bodyB = m_bodies[3];
+    jd.localAnchorA.Set(-10.0f + kWallWidth / 2, 0.0f + kWallHeight - 4);
+    jd.localAnchorB.Set(-0.5f, 0.5f);
+    p1 = jd.bodyA->GetWorldPoint(jd.localAnchorA);
+    p2 = jd.bodyB->GetWorldPoint(jd.localAnchorB);
+    d = p2 - p1;
+    jd.length = d.Length();
+    m_joints[3] = _world->CreateJoint(&jd);
 
-		m_wheel->SetTransform(m_wheel->GetPosition(), -120.0f * b2_pi / 180.0f);
-    [self CreateLeg:-kScale wheelAnchor:wheelAnchor];
-    [self CreateLeg:kScale wheelAnchor:wheelAnchor];
+    jd.bodyA = m_bodies[0];
+    jd.bodyB = m_bodies[1];
+    jd.localAnchorA.Set(0.5f, 0.0f);
+    jd.localAnchorB.Set(-0.5f, 0.0f);;
+    p1 = jd.bodyA->GetWorldPoint(jd.localAnchorA);
+    p2 = jd.bodyB->GetWorldPoint(jd.localAnchorB);
+    d = p2 - p1;
+    jd.length = d.Length();
+    m_joints[4] = _world->CreateJoint(&jd);
+
+    jd.bodyA = m_bodies[1];
+    jd.bodyB = m_bodies[2];
+    jd.localAnchorA.Set(0.0f, 0.5f);
+    jd.localAnchorB.Set(0.0f, -0.5f);
+    p1 = jd.bodyA->GetWorldPoint(jd.localAnchorA);
+    p2 = jd.bodyB->GetWorldPoint(jd.localAnchorB);
+    d = p2 - p1;
+    jd.length = d.Length();
+    m_joints[5] = _world->CreateJoint(&jd);
+
+    jd.bodyA = m_bodies[2];
+    jd.bodyB = m_bodies[3];
+    jd.localAnchorA.Set(-0.5f, 0.0f);
+    jd.localAnchorB.Set(0.5f, 0.0f);
+    p1 = jd.bodyA->GetWorldPoint(jd.localAnchorA);
+    p2 = jd.bodyB->GetWorldPoint(jd.localAnchorB);
+    d = p2 - p1;
+    jd.length = d.Length();
+    m_joints[6] = _world->CreateJoint(&jd);
+
+    jd.bodyA = m_bodies[3];
+    jd.bodyB = m_bodies[0];
+    jd.localAnchorA.Set(0.0f, -0.5f);
+    jd.localAnchorB.Set(0.0f, 0.5f);
+    p1 = jd.bodyA->GetWorldPoint(jd.localAnchorA);
+    p2 = jd.bodyB->GetWorldPoint(jd.localAnchorB);
+    d = p2 - p1;
+    jd.length = d.Length();
+    m_joints[7] = _world->CreateJoint(&jd);
   }
   return self;
 }
@@ -239,8 +164,9 @@ static const CGFloat kScale = 2;
 	int32 positionIterations = 2;
   _world->Step(self.secondsSinceLastTick, velocityIterations, positionIterations);
 
-  m_motorJoint->SetMotorSpeed(m_motorSpeed * self.bassDegrader.value * 4);
-  CGContextTranslateCTM(cx, -m_chassis->GetPosition().x + kWallWidth / 2, -m_chassis->GetPosition().y + kWallHeight / 2);
+  _advance += self.secondsSinceLastTick * 5;
+  m_bodies[0]->ApplyLinearImpulse(b2Vec2(self.bassDegrader.value * 100 * sin(_advance), 0), m_bodies[0]->GetPosition());
+  m_bodies[1]->ApplyLinearImpulse(b2Vec2(self.bassDegrader.value * 100 * sin(_advance), 0), m_bodies[1]->GetPosition());
 
   for (b2Body* b = _world->GetBodyList(); b; b = b->GetNext()) {
     const b2Transform& xf = b->GetTransform();
@@ -258,7 +184,45 @@ static const CGFloat kScale = 2;
           CGContextFillEllipseInRect(cx, CGRectMake(-radius, -radius, radius * 2, radius * 2));
           break;
         }
-          
+
+        case b2Shape::e_edge:
+        {
+          b2EdgeShape* edge = (b2EdgeShape*)f->GetShape();
+          b2Vec2 v1 = b2Mul(xf, edge->m_vertex1);
+          b2Vec2 v2 = b2Mul(xf, edge->m_vertex2);
+
+          CGContextBeginPath(cx);
+          CGContextMoveToPoint(cx, v1.x, v1.y);
+          CGContextAddLineToPoint(cx, v2.x, v2.y);
+
+          CGContextSetStrokeColorWithColor(cx, [NSColor whiteColor].CGColor);
+          CGContextStrokePath(cx);
+        }
+          break;
+
+        case b2Shape::e_chain:
+        {
+          b2ChainShape* chain = (b2ChainShape*)f->GetShape();
+          int32 count = chain->m_count;
+          const b2Vec2* vertices = chain->m_vertices;
+
+          b2Vec2 v1 = b2Mul(xf, vertices[0]);
+          CGContextBeginPath(cx);
+          for (int32 i = 1; i < count; ++i)
+          {
+            b2Vec2 v2 = b2Mul(xf, vertices[i]);
+            if (i == 1) {
+              CGContextMoveToPoint(cx, v1.x, v1.y);
+            }
+            CGContextAddLineToPoint(cx, v2.x, v2.y);
+
+            v1 = v2;
+          }
+          CGContextSetStrokeColorWithColor(cx, [NSColor whiteColor].CGColor);
+          CGContextStrokePath(cx);
+        }
+          break;
+
         case b2Shape::e_polygon: {
           b2PolygonShape* poly = (b2PolygonShape*)f->GetShape();
           int32 vertexCount = poly->m_vertexCount;
@@ -283,6 +247,34 @@ static const CGFloat kScale = 2;
 
       CGContextRestoreGState(cx);
     }
+  }
+
+  for (b2Joint* b = _world->GetJointList(); b; b = b->GetNext()) {
+    CGContextSaveGState(cx);
+
+    b2Body* bodyA = b->GetBodyA();
+    b2Body* bodyB = b->GetBodyB();
+    const b2Transform& xf1 = bodyA->GetTransform();
+    const b2Transform& xf2 = bodyB->GetTransform();
+    b2Vec2 x1 = xf1.p;
+    b2Vec2 x2 = xf2.p;
+    b2Vec2 p1 = b->GetAnchorA();
+    b2Vec2 p2 = b->GetAnchorB();
+
+    switch (b->GetType()) {
+      case e_distanceJoint:
+      {
+        CGContextBeginPath(cx);
+        CGContextMoveToPoint(cx, p1.x, p1.y);
+        CGContextAddLineToPoint(cx, p2.x, p2.y);
+
+        CGContextSetStrokeColorWithColor(cx, [NSColor redColor].CGColor);
+        CGContextStrokePath(cx);
+      }
+        break;
+    }
+    
+    CGContextRestoreGState(cx);
   }
   CGContextRestoreGState(cx);
 }
