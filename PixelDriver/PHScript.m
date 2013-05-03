c1d7bee3975f1c6d9912e5ca653221ddce9bf4f8
jverkoey <jverkoey@gmail.com>
2013-05-02 17:29:22 -0700

    Compile and run scripts.

--- PixelDriver/PHScript.m
+++ PixelDriver/PHScript.m
@@ -16,8 +16,18 @@
 
 #import "PHScript.h"
 
+#import <FScript/FScript.h>
+
+static NSString* const kInitDelim = @"==initialization==";
+static NSString* const kAnimDelim = @"==animation==";
+
 @implementation PHScript {
   NSString* _fullScript;
+
+  NSString* _initScript;
+  NSString* _animationScript;
+
+  FSInterpreter* _interpreter;
 }
 
 - (id)initWithString:(NSString *)string sourceFile:(NSString *)sourceFile {
@@ -32,7 +42,54 @@
 #pragma mark - Compilation
 
 - (void)compileScript {
-  NSLog(@"Compiling %@", self.sourceFile);
+  _initScript = nil;
+  _animationScript = nil;
+  _interpreter = nil;
+
+  NSInteger startOfInit = 0;
+  NSInteger endOfInit = _fullScript.length;
+  NSInteger startOfAnim = 0;
+  NSInteger endOfAnim = _fullScript.length;
+  NSRange rangeOfInitDelim = [_fullScript rangeOfString:kInitDelim options:NSCaseInsensitiveSearch];
+  if (rangeOfInitDelim.length > 0) {
+    startOfInit = NSMaxRange(rangeOfInitDelim) + 1;
+  }
+  NSRange rangeOfAnimDelim = [_fullScript rangeOfString:kAnimDelim options:NSCaseInsensitiveSearch];
+  if (rangeOfAnimDelim.length > 0) {
+    endOfInit = rangeOfAnimDelim.location;
+    startOfAnim = NSMaxRange(rangeOfAnimDelim) + 1;
+  }
+
+  if (startOfAnim < startOfInit) {
+    NSLog(@"Anim must be after init.");
+    return;
+  }
+
+  if (startOfInit > 0) {
+    _initScript = [_fullScript substringWithRange:NSMakeRange(startOfInit, endOfInit - startOfInit)];
+  }
+  if (startOfAnim > 0) {
+    _animationScript = [_fullScript substringWithRange:NSMakeRange(startOfAnim, endOfAnim - startOfAnim)];
+  }
+
+  _interpreter = [[FSInterpreter alloc] init];
+
+  if (nil != _initScript) {
+    FSInterpreterResult *execResult = [_interpreter execute:_initScript];
+    if (![execResult isOK]) {
+      NSLog(@"%@ , character %ld\n", [execResult errorMessage], [execResult errorRange].location);
+    }
+  }
+}
+
+- (void)renderBitmapInContext:(CGContextRef)cx size:(CGSize)size {
+  NSGraphicsContext* previousContext = [NSGraphicsContext currentContext];
+  NSGraphicsContext* graphicsContext = [NSGraphicsContext graphicsContextWithGraphicsPort:cx flipped:NO];
+  [NSGraphicsContext setCurrentContext:graphicsContext];
+
+  [_interpreter execute:_animationScript];
+
+  [NSGraphicsContext setCurrentContext:previousContext];
 }
 
 #pragma mark - Public Methods
